05fbc911c486f900c2bcf04616bec93a05dcfa11
RamonBarros <89661742+RamonBarros@users.noreply.github.com>
2024-05-13 22:11:39 -0300

    Exercicio Tipos de Variaveis em Java

--- TIPOS-VARIAVEIS/src/TiposVariaveis.java
+++ TIPOS-VARIAVEIS/src/TiposVariaveis.java
@@ -1,5 +1,18 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int numero = 5;
+        
+        numero = 10;
+        
+        System.out.println(" ");
+        System.out.println(numero);
+        
+        // o "final" é o equivalente ao const em js.
+        //por convenção constantes devem ser declaradas em CAIXA ALTA(tudo maisculo)
+        final double VALOR_DE_PI = 3.14;
+		
+		// VALOR_DE_PI=3.15; 
+
+        System.out.println(VALOR_DE_PI);
     }
 }
