2ed69c1199754b0d5f1dc5e1137b333138d4f252
RamonBarros <89661742+RamonBarros@users.noreply.github.com>
2024-05-16 15:53:42 -0300

    Códigos do capitulo de controle de fluxo da trilha de java básico.

---
+++ CONTROLE-FLUXO/src/ExemploFor.java
@@ -0,0 +1,15 @@
+// ExemploFor.java
+public class ExemploFor {
+	public static void main(String[] args) {
+		// for(int carneirinhos = 1 ; carneirinhos <=20; carneirinhos ++) {
+		// 	System.out.println(carneirinhos + " - Carneirinho(s)");
+		// }
+
+        String alunos [] =  {"FELIPE","JONAS","JULIA","MARCOS"};
+	
+        //Forma abreviada (ForEach)
+	for(String aluno : alunos) {
+	  System.out.println(aluno);
+	}
+	}
+}
